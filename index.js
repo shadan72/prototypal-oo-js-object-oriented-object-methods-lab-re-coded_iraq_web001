ea01aeeb5060ad8e692b6e14fd4b5e54c04dc61d
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-21 20:34:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function BoardMember(name,homeState,training){
   releasePressStatement(){
     return 'You will see great things from Scuber.'
   }
-  sayHi(){
-    return 
+  BoardMember.prototype.sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${training}.`
   }
 }
